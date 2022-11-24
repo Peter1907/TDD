32c33e635342d2d094a6397ba35b2bb7d0bd02ad
wadaudzai <wadaudzai@gmail.com>
2022-11-24 17:14:18 -0500

    Fizzbuzz method created.

--- solver.rb
+++ solver.rb
@@ -2,15 +2,24 @@ class Solver
   def factorial(num)
     result = 1
     return 'Error: negative number' if num < 0
-    return result if num == 0 || num == 1
-    until num == 0 do
+    return result if [0, 1].include?(num)
+
+    until num == 0
       result *= num
       num -= 1
     end
-    return result
+    result
   end
 
   def reverse(string)
-    return string.reverse
+    string.reverse
+  end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if num % 3 == 0 && num % 5 == 0
+    return 'fizz' if num % 3 == 0
+    return 'buzz' if num % 5 == 0
+
+    num
   end
 end
