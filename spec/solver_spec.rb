a2ba2987ad7cdb500aca5178700feca408cb4722
Peter1907 <peter.nady.1408@gmail.com>
2022-11-24 23:25:57 +0200

    Add tests for the Solver class

---
+++ spec/solver_spec.rb
@@ -0,0 +1,14 @@
+describe 'Solver' do
+  context '#factorial' do
+    solver = Solver.new
+
+    it 'should return the factorial when given a number more than or equal to zero' do
+      expect(solver.factorial(0)).to eql(1)
+      expect(solver.factorial(5)).to eql(120)
+    end
+
+    it 'should return an error when given a negative number' do
+      expect(solver.factorial(-4)).to eql('Error: negative number')
+    end
+  end
+end
